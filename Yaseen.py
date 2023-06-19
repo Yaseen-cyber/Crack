15f89442e3339fcc8d1201eb3fdc9a39647ea5f5
Yaseen Khan <137074308+Yaseen-cyber@users.noreply.github.com>
2023-06-19 18:14:40 +0500

    Create Yaseen.py

---
+++ Yaseen.py
@@ -0,0 +1,8 @@
+import os,platform
+os.system('git pull')
+ 
+Xd=platform.architecture()[0]
+if xd=="32bit":
+    print('Sorry 32 Bit Not Supported...')
+elif xd=="64bit":
+     __import__("Yaseen")
